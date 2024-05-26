6a3298770e04e33ed7c63a187fd51d60df46bce4
Zhou zixuan <sugerzzx@gmail.com>
2024-05-26 18:37:21 +0800

    chore: Update project configuration files and dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          // "@/*": "./src/*",
+          "@screens": "./src/screens",
+          "@components": "./src/components",
+        }
+      }
+    ]
+  ]
 };
