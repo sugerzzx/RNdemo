18a98e4be70f22198c408f5c4ea037a893e38fad
Zhou zixuan <sugerzzx@gmail.com>
2024-06-19 22:38:18 +0800

    chore: Add StatusBar component for translucent status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
 import HomeSCreen from "@screens/HomeScreen";
 import DetailsScreen from '@screens/DetailsScreen';
 import VideoScreen from '@screens/VideoScreen';
@@ -14,14 +15,22 @@ const Stack = createNativeStackNavigator();
 const App: FC<Props> = () => {
   SplashScreen.hide();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeSCreen} options={{ title: 'Home' }} />
-        <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details Title' }} />
-        <Stack.Screen name='Video' component={VideoScreen} options={{ title: 'Video Title' }} />
-        <Stack.Screen name='AiChat' component={AiChat} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar
+        animated={true}
+        translucent={true}
+        backgroundColor='transparent'
+        barStyle={'dark-content'}
+      />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={HomeSCreen} options={{ title: 'Home' }} />
+          <Stack.Screen name='Details' component={DetailsScreen} options={{ title: 'Details Title' }} />
+          <Stack.Screen name='Video' component={VideoScreen} options={{ title: 'Video Title' }} />
+          <Stack.Screen name='AiChat' component={AiChat} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
